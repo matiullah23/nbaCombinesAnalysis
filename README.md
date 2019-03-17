a93910effbed6851efad2b536e699dc9ddb5a3c1
Matiullah Hasher <matiullah@Matiullahs-MacBook-Air.local>
2019-03-17 18:03:50 -0400

    updated the README and minor code touchup

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
 ### nbaCombinesAnalysis
 
-##Initial Plan
+#Initial Plan
 
 Analyzing 2012-2016 NBA Combines using dataset found here : https://data.world/achou/nba-draft-combine-measurements
 
-##Updated Plan
+#Updated Plan
  
 I found raw data on NBA.com that I was able to manually scrape and format in Excel to match the data I already had to expand the years examined from 2012-2016 to 2000-2018.
+
+Project Requirements: 
+Mid-term (due date: March 22nd 11:59 pm)
+Use a dataset of your choosing
+Please send me a summary of the data for review by 3/7
+Must have at least one transformation
+Must have at least one calculated column
+Must have at least three visualizations
+Must tell a story
+Be creative!
--- nba_combine_analysis.R
+++ nba_combine_analysis.R
@@ -1,10 +1,28 @@
 #read in csv's 
-data2012 <- read.csv(file="2012_nba_draft_combine.csv", header=TRUE, sep=",")
-data2013 <- read.csv(file="2013_nba_draft_combine.csv", header=TRUE, sep=",")
-data2014 <- read.csv(file="2014_nba_draft_combine.csv", header=TRUE, sep=",")
-data2015 <- read.csv(file="2015_nba_draft_combine.csv", header=TRUE, sep=",")
-data2016 <- read.csv(file="2016_nba_draft_combine.csv", header=TRUE, sep=",")
+data2012 <- read.csv(file="2012_nba_draft_combine.csv", header=TRUE, sep=",", check.names = FALSE, fileEncoding = 'UTF-8-BOM')
+data2013 <- read.csv(file="2013_nba_draft_combine.csv", header=TRUE, sep=",", check.names = FALSE, fileEncoding = 'UTF-8-BOM')
+data2014 <- read.csv(file="2014_nba_draft_combine.csv", header=TRUE, sep=",", check.names = FALSE, fileEncoding = 'UTF-8-BOM')
+data2015 <- read.csv(file="2015_nba_draft_combine.csv", header=TRUE, sep=",", check.names = FALSE, fileEncoding = 'UTF-8-BOM')
+data2016 <- read.csv(file="2016_nba_draft_combine.csv", header=TRUE, sep=",", check.names = FALSE, fileEncoding = 'UTF-8-BOM')
 
 #combine the 4 datasets to create working dataset containing data for all 4 yeears
 dataset <- rbind(data2012, data2013, data2014, data2015, data2016)
+
+#drop the columns not being used (since these were not found in the other source of data for other combine years)
+dataset <- dataset[,-1]
+dataset <- dataset[,-9]
+dataset <- dataset[,-9]
+
+#bring in the data that was manually scraped from nba.com 
+rest_of_data <- read.csv(file="2001to2011_2017_2018_nba_draft_combine.csv", header=TRUE, sep=",", check.names = FALSE, fileEncoding = 'UTF-8-BOM')
+
+
+#combine the two datasets to create master dataset
+df <- rbind(dataset, rest_of_data)
+
+#export to CSV 
+write.csv(df, file="nba_combine_data_merged.csv")
+
+
+
 
